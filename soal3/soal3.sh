1db13c0b2ae4170aadc0b3b86238c153c8ebaed3
redruby1 <56338326+redruby1@users.noreply.github.com>
2020-02-21 17:20:12 +0700

    Add files via upload

---
+++ soal3/soal3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir=`pwd`
+
+for n in $(seq 1 28)
+do
+        wget -O $dir/downloads/pdkt_kusuma_$n -a $dir/wget.log https://loremfli$
+done
+
+for file in $(fdupes -r -f $dir/downloads | grep -v '^$')
+do
+        mv $file $dir/duplicate/duplicate_$((n++))
+done
+
+for data in  $(ls $dir/downloads)
+do
+        mv $data $dir/kenangan/kenangan_$((i++))
+done
+
+grep "Location" $dir/wget.log >> $dir/location.log
