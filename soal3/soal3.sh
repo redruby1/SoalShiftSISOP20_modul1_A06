f45b9015dc05202d97e325cd9b42bda3d50d2bbe
redruby1 <56338326+redruby1@users.noreply.github.com>
2020-02-21 17:29:00 +0700

    Update soal3.sh

--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -4,7 +4,7 @@ dir=`pwd`
 
 for n in $(seq 1 28)
 do
-        wget -O $dir/downloads/pdkt_kusuma_$n -a $dir/wget.log https://loremfli$
+        wget -O $dir/downloads/pdkt_kusuma_$n -a $dir/wget.log https://loremflickr.com/320/240/cat
 done
 
 for file in $(fdupes -r -f $dir/downloads | grep -v '^$')
