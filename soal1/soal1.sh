f668b99235aba9d921d4769ebbb495e35271dbc7
redruby1 <56338326+redruby1@users.noreply.github.com>
2020-02-29 01:30:37 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -4,11 +4,11 @@ echo "Soal Shift No 1"
 echo "  "
 
 echo "Wilayah bagian (region) yang memiliki keuntungan paling sedikit"
-awk -F'\t' 'NR>1{a[$13]+=$21} END{for(i in a) print a[i]"\t",i}' Sample-Superstore.tsv | sort -g | awk -F'\t' '{print $2}' | head -1
+awk -F'\t' 'NR>1{a[$13]+=$21} END{for(i in a) print a[i]",",i}' Sample-Superstore.tsv | sort -g | awk -F',' '{print $2}' | head -1
 echo "  "
 
 echo "2 negara bagian (state) yang memiliki keuntungan paling sedikit di region$
-awk -F'\t' '{if($13=="Central") a[$11]+=$21} END{for(i in a) print a[i]"\t",i}' Sample-Superstore.tsv | sort -g | awk -F',' '{print $2}' | head -2
+awk -F'\t' '{if($13=="Central") a[$11]+=$21} END{for(i in a) print a[i]",",i}' Sample-Superstore.tsv | sort -g | awk -F',' '{print $2}' | head -2
 echo "  "
 
 echo "10 produk yang memiliki keuntungan paling sedikit di Texas atau Illinois"
