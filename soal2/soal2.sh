dd32796ad26cee335d474f63826f2527c3e50f00
Anisa A <56338326+redruby1@users.noreply.github.com>
2020-02-29 10:40:39 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -3,10 +3,10 @@
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1)
 
 re=[0-9a-zA-Z]+
+
 while ! [[ "password" =~ ${re} ]]
 do
 	password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1)
-
 done
 
 folder=`pwd`
@@ -15,6 +15,7 @@ echo Masukkan Nama File yang Diinginkan
 read judul
 
 if [[ $judul =~ ^[+-]?[a-zA-Z]+$ ]]; 
-then echo "$password" >> $folder/$judul.txt
-else echo Maaf tidak bisa, file hanya dinamai dengan format alfabet
+	then echo "$password" >> $folder/$judul.txt
+else 
+	echo "Maaf tidak bisa, file hanya dinamai dengan format alfabet"
 fi
